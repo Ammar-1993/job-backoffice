3a3b947da7f3d18f6bee36879b4589f2a8dfe65a
Ammar-1993 <ammaralnggar@gmail.com>
2026-06-11 18:06:18 +0300

    fix: update JobApplication status update to use ApplicationStatus enum

--- app/Http/Controllers/JobApplicationController.php
+++ app/Http/Controllers/JobApplicationController.php
@@ -82,7 +82,7 @@ class JobApplicationController extends Controller
     {
         $jobApplication = JobApplication::findOrFail($id);
         $jobApplication->update([
-            'status' => \App\Enums\JobStatus::from($request->input('status')),
+            'status' => \App\Enums\ApplicationStatus::from($request->input('status')),
         ]);
 
         if($request->query('redirectToList') == 'false'){
