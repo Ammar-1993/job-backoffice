eade027c2bac7948ad18ce56ea7d026d3a8721a4
Ammar-1993 <ammaralnggar@gmail.com>
2026-06-16 05:19:18 +0300

    fix: update job type display to support BackedEnum values in index view

--- resources/views/job-vacancy/index.blade.php
+++ resources/views/job-vacancy/index.blade.php
@@ -98,10 +98,11 @@
                                                         'Remote' => 'bg-emerald-50 text-emerald-700 border-emerald-200',
                                                         'Hybrid' => 'bg-purple-50 text-purple-700 border-purple-200',
                                                     ];
-                                                    $style = $typeStyles[$jobVacancy->type] ?? 'bg-gray-50 text-gray-700 border-gray-200';
+                                                    $typeValue = $jobVacancy->type instanceof \BackedEnum ? $jobVacancy->type->value : $jobVacancy->type;
+                                                    $style = $typeStyles[$typeValue] ?? 'bg-gray-50 text-gray-700 border-gray-200';
                                                 @endphp
                                                 <span class="px-2.5 py-1 inline-flex text-xs font-bold rounded-lg border {{ $style }}">
-                                                    {{ $jobVacancy->type }}
+                                                    {{ $typeValue }}
                                                 </span>
                                             </div>
                                         </div>
@@ -195,10 +196,11 @@
                                                 'Remote' => 'bg-emerald-50 text-emerald-700 border-emerald-200',
                                                 'Hybrid' => 'bg-purple-50 text-purple-700 border-purple-200',
                                             ];
-                                            $styleMobile = $typeStylesMobile[$jobVacancy->type] ?? 'bg-gray-50 text-gray-700 border-gray-200';
+                                            $typeValueMobile = $jobVacancy->type instanceof \BackedEnum ? $jobVacancy->type->value : $jobVacancy->type;
+                                            $styleMobile = $typeStylesMobile[$typeValueMobile] ?? 'bg-gray-50 text-gray-700 border-gray-200';
                                         @endphp
                                         <span class="px-2.5 py-1 inline-flex text-xs font-bold rounded-lg border {{ $styleMobile }}">
-                                            {{ $jobVacancy->type }}
+                                            {{ $typeValueMobile }}
                                         </span>
                                     </div>
                                 </div>
